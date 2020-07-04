2ec43b30780268c929fc885eb9b574ea4eba003b
calugatop <50307913+calugatop@users.noreply.github.com>
2020-07-04 12:49:32 +0300

    040720201250

---
+++ res/search.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'4' => '2.html',
+		'5' => 'zdorovie.html',
+		'6' => 'text.html',
+		'7' => 'page-7.html',
+		'8' => 'page-8.html',
+		'9' => 'love.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+
+);
+
+// End of file search.inc.php
